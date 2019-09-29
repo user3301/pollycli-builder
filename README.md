d6c6d7e77a826f7e9697dc52648be52639699d39
user3301 <stan_gai@hotmail.com>
2019-09-29 15:45:05 +1000

    make main method docker friendly

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # TVlPQi5Db2Rl
 [![Language](https://img.shields.io/badge/language-csharp-green.svg)](https://docs.microsoft.com/en-us/dotnet/csharp/)
 [![Platform](https://img.shields.io/badge/.Net%20Core-3.0-brightgreen)](https://dotnet.microsoft.com/download/dotnet-core/3.0)
+[![Build Status](https://travis-ci.org/user3301/TVlPQi5Db2Rl.svg?branch=master)](https://travis-ci.org/user3301/TVlPQi5Db2Rl)
--- TVlPQi5Db2Rl/Program.cs
+++ TVlPQi5Db2Rl/Program.cs
@@ -2,6 +2,7 @@
 using FileIOProcessor.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using TaxCalculationService;
 
@@ -9,7 +10,9 @@ namespace TVlPQi5Db2Rl
 {
     class Program
     {
-        static async Task Main()
+        private static readonly AutoResetEvent waitHandle = new AutoResetEvent(false);
+
+        static void Main()
         {
             //set up DI
             var serviceProvider = new ServiceCollection()
@@ -19,42 +22,57 @@ namespace TVlPQi5Db2Rl
             var CsvFileProcessor = serviceProvider.GetService<IFileIOService>();
             Console.WriteLine("Welcome To Tax Calculator!");
 
-            while (true)
+            _ = Task.Run(async () =>
+                  {
+                      while (true)
+                      {
+                          try
+                          {
+                              Console.WriteLine("please specify your csv file path in full or type 'exit' to stop:");
+                              string line = Console.ReadLine();
+                              if (line == "exit")
+                              {
+                                  Console.WriteLine("Bye!");
+                                  break;
+                              }
+                              Console.WriteLine("Your csv input:");
+                              var employees = await CsvFileProcessor.ReadAsync(line);
+                              employees.ForEach(e => Console.WriteLine(e.ToString()));
+                              Console.WriteLine("Tax calculation outcome:");
+                              foreach (var employee in employees)
+                              {
+                                  var handler = new TaxCalculationHandler(employee);
+                                  var payslip = handler.GenerateMonthlyPaySlip();
+                                  Console.WriteLine(payslip.ToString());
+                              }
+                          }
+                          catch (Exception ex)
+                          {
+                              Console.WriteLine(ex.Message);
+                              if (ex.InnerException != null)
+                              {
+                                  Console.WriteLine("Inner Exception: ");
+                                  Console.WriteLine(ex.InnerException.Message);
+                              }
+                              Console.WriteLine("Stack Trace: ");
+                              Console.WriteLine(ex.StackTrace);
+                          }
+                      }
+                  }
+
+            );
+
+            // Handle Control+C or Control+Break
+            Console.CancelKeyPress += (o, e) =>
             {
-                try
-                {
-                    Console.WriteLine("please specify your csv file path in full or type 'exit' to stop:");
-                    string line = Console.ReadLine();
-                    if (line == "exit")
-                    {
-                        Console.WriteLine("Bye!");
-                        break;
-                    }
-                    Console.WriteLine("Your csv input:");
-                    var employees = await CsvFileProcessor.ReadAsync(line);
-                    employees.ForEach(e => Console.WriteLine(e.ToString()));
-                    Console.WriteLine("Tax calculation outcome:");
-                    foreach (var employee in employees)
-                    {
-                        var handler = new TaxCalculationHandler(employee);
-                        var payslip = handler.GenerateMonthlyPaySlip();
-                        Console.WriteLine(payslip.ToString());
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    if (ex.InnerException != null)
-                    {
-                        Console.WriteLine("Inner Exception: ");
-                        Console.WriteLine(ex.InnerException.Message);
-                    }
-                    Console.WriteLine("Stack Trace: ");
-                    Console.WriteLine(ex.StackTrace);
-                }
-            }
+                Console.WriteLine("Exit");
 
+                // Allow the manin thread to continue and exit...
+                waitHandle.Set();
+            };
 
+            // Wait
+            waitHandle.WaitOne();
         }
     }
 }
